d0a28e9878655b143cbabd9aa57e1445e738fc97
dviggiano <dviggiano@wisc.edu>
2022-01-20 01:23:16 -0500

    commiting before switch to main for docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:16
+
+WORKDIR /var/app
+
+COPY package.json /var/app
+
+RUN npm install
+
+COPY . /var/app
+
+CMD ["npm", "run", "dev"]
--- queues/email.queue.ts
+++ queues/email.queue.ts
@@ -2,12 +2,15 @@ import Bull from "bull";
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { ExpressAdapter } from "@bull-board/express";
+import process from "process";
 
 import sendNewEmail from "../processes/email.process";
 import addToGroups from "../processes/groups.process";
 import addToSpreadsheet from "../processes/spreadsheet.process";
 
-const emailQueue = new Bull("emailQueue");
+const emailQueue = new Bull("emailQueue", {
+  redis: process.env.REDIS_URL
+});
 
 const serverAdapter = new ExpressAdapter();
 
