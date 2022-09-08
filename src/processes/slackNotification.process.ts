6877c4c0e47e2909a16ba006e12b5781bc3e6062
eeshaan <eeshaan@cs.wisc.edu>
2022-09-08 14:56:58 -0500

    add Slack `WebClient`

--- src/processes/slackNotification.process.ts
+++ src/processes/slackNotification.process.ts
@@ -1,4 +1,5 @@
 import { Job } from "bull";
+// import { WebClient } from "@slack/web-api";
 
 import { Subscription, Unsubscription } from "../common/types";
 
@@ -15,10 +16,28 @@ const isSubscription = (
 const slackNotification = async (
   job: Job<Subscription> | Job<Unsubscription>
 ) => {
+  // // Read a token from the environment variables
+  // const token = process.env.SLACK_TOKEN;
+
+  // // Initialize
+  // const web = new WebClient(token);
+
   if (isSubscription(job)) {
     console.log(`Subscription: ${job.data.firstName}`);
+
+    // using the Slack JavaScript SDK, send a message to the #subscriptions channel
+    // https://slack.dev/node-slack-sdk/web-api
+    // https://api.slack.com/methods/chat.postMessage
+    // await web.chat.postMessage({
+    //   channel: "#subscriptions",
+    //   text: `New subscription: ${job.data.firstName} ${job.data.email}`
+    // });
   } else {
     console.log(`Unsubscription: ${job.data.email}`);
+    // await web.chat.postMessage({
+    //   channel: "#subscriptions",
+    //   text: `Unsubscription: ${job.data.email}`
+    // });
   }
 };
 
